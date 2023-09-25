10e1cdd2b37ff7310a9d4521588f341b7bf773d0
Lainey <dev.lainey@gmail.com>
2023-09-24 22:01:28 -0400

    add footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+//----- IMPORTS -----//
+
+//CSS
+import '../assets/css/Footer.css';
+
+//----- EXPORTS -----//
+export default function Footer() {
+
+    //Return
+    return (
+        //---------- FOOTER ----------//
+        <div className='footer-container'>
+            <footer>
+                <p className='tag'>MADE WITH PASSION BY LAINEY CREIGHTON</p>
+            </footer>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
+//----- IMPORTS -----//
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Pages
+import Home from './pages/Home.jsx';
+import Dev from './pages/Dev.jsx';
+
+//----- EXPORTS -----//
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dev",
+        element: <Dev />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
