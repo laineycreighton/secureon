3c80aef5622c4798d4b58ebcde542a905e7ddcb5
Lainey <dev.lainey@gmail.com>
2023-09-24 22:17:54 -0400

    add password content

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -2,35 +2,20 @@
 import { Link } from 'react-router-dom';
 
 //CSS
-import '../assets/css/Footer.css';
-
-//Images
-import github from '../assets/images/github.svg';
-import linkedin from '../assets/images/linkedin.svg';
-import email from '../assets/images/email.svg';
+import '../assets/css/Password.css';
 
 //----- EXPORTS -----//
-export default function Footer() {
-
-    const mygithub = 'https://github.com/laineycreighton';
-    const mylinkedin = 'https://www.linkedin.com/in/lainey-creighton/';
-    const myemail = 'dev.lainey@gmail.com';
+export default function Password() {
 
     //Return
     return (
-        //---------- FOOTER ----------//
-        <div className='footer-container'>
-            <footer>
-                <div>
-                    <p className='tag'>MADE WITH <span className='passion'>Passion</span> BY <Link className='my-name' to="/dev">LAINEY CREIGHTON</Link></p>
-                </div>
-                <div className='separator'></div>
-                <div className='contact-icons'>
-                    <a href={mygithub} target='_blank' ><img src={github} alt='github' /></a>
-                    <a href={mylinkedin} target='_blank' ><img src={linkedin} alt='linkedin' /></a>
-                    <a href={myemail} target='_blank' ><img src={email} alt='email' /></a>
-                </div>
-            </footer>
+        //---------- PASSWORD ----------//
+        <div className='password-container'>
+            <h2>YOUR PASSWORD</h2>
+            <div>
+                <p></p>
+            </div>
+            <button>NEW PASSWORD</button>
         </div>
     )
 }
